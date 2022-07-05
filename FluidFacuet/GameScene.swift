ce1f9ae5d07d2a52683ad26e747f0e5f0849ae64
Paolo ferrari <paoloferrariapple@gmail.com>
2022-07-05 14:58:44 +0200

    Created project base

--- FluidFacuet/GameScene.swift
+++ FluidFacuet/GameScene.swift
@@ -1,88 +1,70 @@
-//
-//  GameScene.swift
-//  FluidFacuet
-//
-//  Created by user on 05/07/22.
-//
-
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var backGround: SKSpriteNode!
+    var faucet: SKSpriteNode!
+    var openFaucet: Bool = false
+    var droplet: SKSpriteNode!
+    
     
     override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame.inset(by: UIEdgeInsets(top: 167, left: 235, bottom: 50, right: 235)))
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //Background
+        let backGround = SKSpriteNode(imageNamed: "Background")
+        backGround.position = CGPoint(x: 0, y: 0)
+        backGround.scale(to: CGSize(width: 750, height: 1334))
+        backGround.zPosition = -1
+        addChild(backGround)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        //faucet button
+        faucet = SKSpriteNode (imageNamed: "Arrow")
+        faucet.position = CGPoint(x:80, y:50)
+        faucet.size = CGSize (width: 55, height: 55)
+        faucet.name = "Left"
+        faucet.zRotation = .pi / 2
+        faucet.zPosition = 10
+        addChild(faucet)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch: AnyObject in touches {
+            let location = touch.location(in: self)
+            let node = self.atPoint(location)
+            
+            if (node.name == "Left") {
+                openFaucet = true
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for touch: AnyObject in touches {
+            let location = touch.location(in: self)
+            let node = self.atPoint(location)
+            
+            if (node.name == "Left") {
+                openFaucet = false
+            }
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
+        if openFaucet {
+            droplet = SKSpriteNode(imageNamed: "Droplet")
+            droplet.position = CGPoint(x: 70, y: 100)
+            droplet.scale(to: CGSize(width: 10, height: 10))
+            droplet.physicsBody = SKPhysicsBody(rectangleOf: droplet.size)
+            droplet.physicsBody?.restitution = 0.6
+            droplet.physicsBody?.friction = 0
+            addChild(droplet)
+        }
     }
 }
+
+
+// 375*2 = 750
+// 667*2 = 1334
